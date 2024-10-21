90dfb81f345425ba94a02a131acefbeb046e94af
hiroro-work <hiroro@sonicgarden.jp>
2024-10-21 09:46:16 +0900

    MantineのModalsが使えるように

--- app/utils/mantine/provider.tsx
+++ app/utils/mantine/provider.tsx
@@ -2,6 +2,7 @@ import {
   ColorSchemeScript as _ColorSchemeScript,
   MantineProvider as _MantineProvider,
 } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import type { ReactNode } from 'react';
 
@@ -10,6 +11,6 @@ export const ColorSchemeScript = () => <_ColorSchemeScript />;
 export const MantineProvider = ({ children }: { children: ReactNode }) => (
   <_MantineProvider>
     <Notifications />
-    {children}
+    <ModalsProvider>{children}</ModalsProvider>
   </_MantineProvider>
 );
