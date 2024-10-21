a8c3d339e0e666e8f29e8f499f6015cc17dd7bd6
hiroro-work <hiroro@sonicgarden.jp>
2024-10-21 10:13:54 +0900

    Firebaseアプリインスタンスの初期化

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,4 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { initializeApp } from './utils/firebase/app.js';
 
-// import * as logger from 'firebase-functions/logger';
-// import { onRequest } from 'firebase-functions/v2/https';
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+process.env.TZ = 'Asia/Tokyo';
+initializeApp();
