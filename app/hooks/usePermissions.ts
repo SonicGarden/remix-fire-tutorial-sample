9ef403af456667f2312f820fe922e942b3907ced
hiroro-work <hiroro@sonicgarden.jp>
2024-10-21 14:14:19 +0900

    管理者ユーザー以外が管理画面にアクセスできないように

---
+++ app/hooks/usePermissions.ts
@@ -0,0 +1,15 @@
+import { useCallback } from 'react';
+import { useAuth } from '~/contexts/auth';
+
+export const usePermissions = () => {
+  const { role } = useAuth();
+  const validatePathPermission = useCallback(
+    (path: string) => {
+      if (/^\/admin\/?$/.test(path) && role === 'admin') return true;
+      return false;
+    },
+    [role],
+  );
+
+  return { validatePathPermission };
+};
--- app/layouts/AdminLayout.tsx
+++ app/layouts/AdminLayout.tsx
@@ -1,8 +1,12 @@
+import { useNavigate, useLocation } from '@remix-run/react';
+import { useEffect } from 'react';
 import {
   ResponsiveLayout,
   useResponsiveLayoutContext,
 } from '~/components/layouts/ResponsiveLayout';
-import { withAuth } from '~/contexts/auth';
+import { LoadingScreen } from '~/components/screens/LoadingScreen';
+import { withAuth, useAuth } from '~/contexts/auth';
+import { usePermissions } from '~/hooks/usePermissions';
 import { AccountMenu } from './_components/AccountMenu';
 import { AdminNavMenu } from './_components/AdminNavMenu';
 import { AdminTitle } from './_components/AdminTitle';
@@ -10,6 +14,29 @@ import type { ReactNode } from 'react';
 
 export const AdminLayout = withAuth(
   ({ children }: { children: ReactNode }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const currentPath = location.pathname + location.search;
+    const { signedIn } = useAuth();
+    const { validatePathPermission } = usePermissions();
+    const validatedPathPermission = validatePathPermission(currentPath);
+
+    useEffect(() => {
+      if (signedIn === false)
+        navigate(
+          {
+            pathname: '/admin/sign-in',
+            search: `?redirect=${currentPath}`,
+          },
+          { replace: true },
+        );
+      if (signedIn === true && !validatedPathPermission)
+        navigate('/', { replace: true });
+    }, [signedIn, validatedPathPermission, currentPath, navigate]);
+
+    if (!signedIn) return <LoadingScreen />;
+    if (!validatedPathPermission) return <LoadingScreen />;
+
     return (
       <ResponsiveLayout
         header={{
