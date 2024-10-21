71341bbe197d1f1c025493fe94127d3c1b977372
hiroro-work <hiroro@sonicgarden.jp>
2024-10-21 15:16:30 +0900

    ルートページにアクセスしたら書籍一覧に遷移するように

--- app/routes/_public._index.tsx
+++ app/routes/_public._index.tsx
@@ -1,5 +1,10 @@
-import { Center } from '@mantine/core';
+import { replace } from '@remix-run/react';
+import { LoadingScreen } from '~/components/screens/LoadingScreen';
+
+export async function clientLoader() {
+  return replace('/books');
+}
 
 export default function Index() {
-  return <Center>Home</Center>;
+  return <LoadingScreen />;
 }
