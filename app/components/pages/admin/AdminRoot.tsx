da290b334060d9bb99c3bfd809e49a046c49c455
hiroro-work <hiroro@sonicgarden.jp>
2024-10-21 15:03:21 +0900

    clientLoaderでリダイレクトするように

--- app/components/pages/admin/AdminRoot.tsx
+++ app/components/pages/admin/AdminRoot.tsx
@@ -1,13 +1,5 @@
-import { useNavigate } from '@remix-run/react';
-import { useEffect } from 'react';
 import { LoadingScreen } from '~/components/screens/LoadingScreen';
 
 export const AdminRoot = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate('/admin/books', { replace: true });
-  }, [navigate]);
-
   return <LoadingScreen />;
 };
--- app/routes/admin._index.tsx
+++ app/routes/admin._index.tsx
@@ -1,4 +1,9 @@
+import { replace } from '@remix-run/react';
 import { AdminRoot } from '~/components/pages/admin/AdminRoot';
+
+export async function clientLoader() {
+  return replace('/admin/books');
+}
 
 export default function AdminRootPage() {
   return <AdminRoot />;
