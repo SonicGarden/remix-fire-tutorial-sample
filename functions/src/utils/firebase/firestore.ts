6d38b289e25f5ca47a46b279b3a2bf02d3be413a
hiroro-work <hiroro@sonicgarden.jp>
2024-10-21 10:14:10 +0900

    ユーザーデータ用のユーティリティ関数作成

---
+++ functions/src/utils/firebase/firestore.ts
@@ -0,0 +1,47 @@
+import {
+  FieldValue,
+  getFirestore as _getFirestore,
+} from 'firebase-admin/firestore';
+import type { WithId } from '@local/shared';
+import type {
+  DocumentData,
+  FirestoreDataConverter,
+  Firestore,
+  Timestamp,
+  WithFieldValue,
+} from 'firebase-admin/firestore';
+
+let firestore: Firestore;
+const getFirestore = () => {
+  if (firestore) return firestore;
+
+  firestore = _getFirestore();
+  firestore.settings({
+    preferRest:
+      process.env.NODE_ENV === 'test' ||
+      process.env.FUNCTIONS_EMULATOR === 'true'
+        ? false
+        : true,
+  });
+  return firestore;
+};
+
+const { serverTimestamp: _severTimestamp } = FieldValue;
+
+const serverTimestamp = () => _severTimestamp() as Timestamp;
+
+const getConverter = <T extends DocumentData>(): FirestoreDataConverter<
+  WithId<T>,
+  T
+> => ({
+  toFirestore: (data) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { id, ...rest } = data;
+    return rest as WithFieldValue<T>;
+  },
+  fromFirestore: (snapshot) => {
+    return { id: snapshot.id, ...snapshot.data() } as WithId<T>;
+  },
+});
+
+export { serverTimestamp, getConverter, getFirestore, Timestamp };
