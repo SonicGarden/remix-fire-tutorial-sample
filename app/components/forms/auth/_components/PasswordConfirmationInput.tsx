44a243e9b9167e773357c4ac1700177399245e49
hiroro-work <hiroro@sonicgarden.jp>
2024-10-21 09:39:20 +0900

    サインアップフォームを作成

---
+++ app/components/forms/auth/_components/PasswordConfirmationInput.tsx
@@ -0,0 +1,28 @@
+import { PasswordInput as MantinePasswordInput } from '@mantine/core';
+import { z } from 'zod';
+import type { PasswordInputProps } from '@mantine/core';
+import type { UseFormReturnType } from '@mantine/form';
+
+export const passwordConfirmationValidation = z
+  .string()
+  .min(1, { message: '入力してください' });
+
+export const PasswordConfirmationInput = <
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Form extends UseFormReturnType<any>,
+>({
+  form,
+  ...props
+}: { form: Form } & Omit<PasswordInputProps, 'form'>) => {
+  const name = 'passwordConfirmation';
+  const label = 'パスワード(確認)';
+
+  return (
+    <MantinePasswordInput
+      label={label}
+      aria-label={label}
+      {...props}
+      {...form.getInputProps(name)}
+    />
+  );
+};
