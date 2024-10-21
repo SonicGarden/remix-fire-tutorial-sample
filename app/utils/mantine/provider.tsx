f92217af5e71bb839ec04032e80796f16d981472
hiroro-work <hiroro@sonicgarden.jp>
2024-10-21 09:34:12 +0900

    MantineのNotificationsが使えるように

--- app/utils/mantine/provider.tsx
+++ app/utils/mantine/provider.tsx
@@ -2,10 +2,14 @@ import {
   ColorSchemeScript as _ColorSchemeScript,
   MantineProvider as _MantineProvider,
 } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import type { ReactNode } from 'react';
 
 export const ColorSchemeScript = () => <_ColorSchemeScript />;
 
 export const MantineProvider = ({ children }: { children: ReactNode }) => (
-  <_MantineProvider>{children}</_MantineProvider>
+  <_MantineProvider>
+    <Notifications />
+    {children}
+  </_MantineProvider>
 );
